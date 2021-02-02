e4b88037a3a429cab752696109c8ee5fba9cf0bd
Erkan Akkoç <akkoc.erkan@gmail.com>
2021-02-02 03:25:22 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
