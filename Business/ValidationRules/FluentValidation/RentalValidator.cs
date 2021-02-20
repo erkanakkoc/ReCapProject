89401f678b4960878429cc66be1fa509fbfa083b
Erkan Akkoç <akkoc.erkan@gmail.com>
2021-02-20 17:53:31 +0300

    Update v1.8.2

--- Business/ValidationRules/FluentValidation/RentalValidator.cs
+++ Business/ValidationRules/FluentValidation/RentalValidator.cs
@@ -15,7 +15,6 @@ namespace Business.ValidationRules.FluentValidation
         {
             RuleFor(r => r.RentDate).NotEmpty();
             RuleFor(r => r.ReturnDate).GreaterThanOrEqualTo(r => r.RentDate).When(r=>r.ReturnDate.HasValue).WithMessage("Rent Date can't bigger than Return Date");
-            RuleFor(r => r.CarId).Equal(r => r.CarId);
         }
 
     }
